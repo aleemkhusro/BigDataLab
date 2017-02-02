8ef3d43e0355c94f5a96a0f1ebf90929655c1dfa
talha970 <talha970@gmail.com>
2017-02-01 23:14:37 -0600

    added files

---
+++ Spark WordCount/src/main/scala/SparkWordCount.scala
@@ -0,0 +1,35 @@
+
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+
+object SparkWordCount {
+
+  def main(args: Array[String]) {
+
+    System.setProperty("hadoop.home.dir","C:\\winutils");
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[1]")
+
+    val sc=new SparkContext(sparkConf)
+
+    val input=sc.textFile("input")
+
+    val wc=input.flatMap(line=>{line.split(" ")}).map(  word=>(word.length(),1)       ).cache()
+
+    val output=wc.reduceByKey(_+_)
+
+    output.saveAsTextFile("output")
+
+    val o=output.collect()
+
+    var s:String="Words:Count \n"
+    o.foreach{case(word,count)=>{
+
+      s+=word+" : "+count+"\n"
+
+    }}
+
+  }
+
+}
